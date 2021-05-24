b952ff1eac213de63655530de75206fabe4d2a3b
Carsten Andrich <carsten@andrich.name>
2021-05-24 10:41:47 +0200

    add threaded_read example, remove clone_read_write example
    (cherry picked from commit 8de8021f45581e75cad79ecadf8f89e4a93a9bef)

---
+++ examples/threaded_read.rs
@@ -0,0 +1,50 @@
+extern crate serial;
+
+use std::env;
+use std::io::Read;
+use std::thread;
+use std::time::{Duration, Instant};
+use serial::SerialPort;
+
+const NUM_IO_OPS: usize = 50;
+
+fn main() {
+	let args: Vec<String> = env::args().collect();
+	if args.len() != 2 {
+		#[cfg(unix)]
+		println!("Usage: {} /dev/ttyN", args[0]);
+		#[cfg(windows)]
+		println!("Usage: {} COMn", args[0]);
+		return;
+	}
+
+	// open serial port and clone once
+	let mut ser = SerialPort::open(
+			&args[1], Some(Duration::from_millis(1000))
+		).expect("opening serial port failed");
+	let mut ser_clone = ser.try_clone().expect("cloning serial port failed");
+
+	let start = Instant::now();
+	let mut threads = Vec::new();
+
+	// read from cloned serial port in separate thread
+	threads.push(thread::spawn(move || {
+		let mut buf = [0u8; 1024];
+		for _ in 0..NUM_IO_OPS {
+			let res = ser_clone.read(&mut buf);
+			println!("<C {:?} {:?}", start.elapsed(), res);
+		}
+	}));
+
+	// read from original serial port in main thread
+	let mut buf = [0u8; 1024];
+	for _ in 0..NUM_IO_OPS {
+		let res = ser.read(&mut buf);
+		println!("<M {:?} {:?}", start.elapsed(), res);
+	}
+
+	// join all spawned threads
+	for t in threads {
+		t.join().unwrap();
+	}
+}
